c0604693f447cc6325f9c04f2c62d3f7595a8cb7
Roberts Kalnins <rkalnins314@gmail.com>
2019-04-21 22:42:03 -0400

    Add input option parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
-use std::io::{stdin};
-use structopt::StructOpt;
-
+use std::io::{stdin, stdout, Write};
 
 fn main() {
-    let args = Cli::from_args();
-}
+    println!("User management system");
+    let mut arg: i8 = 0;
 
-#[derive(StructOpt)]
-struct Cli {
-    operation: String,
+    while arg != 5 {
+        let mut input_text = String::new();
+
+        println!();
+        println!("1. Add user");
+        println!("2. View User");
+        println!("3. Find Users");
+        println!("4. Remove User");
+        println!("5. Quit");
+        print!("> ");
+
+        stdout().flush().expect("failed to flush stdout");
+
+        stdin()
+            .read_line(&mut input_text)
+            .expect("failed to read from stdin");
+
+        let trimmed = input_text.trim();
+        let arg = match trimmed.parse::<i8>() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("enter valid input");
+                continue;
+            }
+        };
+    }
 }
 
 struct User {
@@ -16,7 +37,5 @@ struct User {
     username: String,
     email: String,
     age: i8,
-    id: i64
+    id: i64,
 }
-
-
